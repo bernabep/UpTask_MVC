2d565a40e0eb3ab6f5c11c7036daa276fac9233f
Bernabé Pando Fernández <bernabepando@gmail.com>
2024-12-31 19:21:01 +0100

    clase 638 eliminando tareas

--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = mysqli_connect('localhost', 'root', 'root', 'uptask_mvc');
+$db = mysqli_connect('192.168.0.90', 'remote', 'root', 'uptask_mvc');
 
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
