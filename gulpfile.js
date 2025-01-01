eb17a15147c5e522ee2908190cdcfc9365942d75
Bernabep <bernabepando@gmail.com>
2025-01-01 23:21:13 +0100

    pc principal

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,14 @@ const sass = gulpSass(dartSass)
 
 const paths = {
     scss: 'src/scss/**/*.scss',
-    js: 'src/js/**/*.js'
+    js: 'src/js/**/*.js',
+    images: 'src/img/**/*.{jpg,jpeg,png,gif,svg}'
+}
+
+export function images( done ) {
+    src(paths.images)
+        .pipe(dest('./public/build/img')); // Puedes añadir más pipes aquí si necesitas optimizar las imágenes
+    done();
 }
 
 export function css( done ) {
@@ -29,6 +36,7 @@ export function js( done ) {
 export function dev() {
     watch( paths.scss, css );
     watch( paths.js, js );
+    watch( paths.images, images );
 }
 
-export default series( js, css, dev )
+export default series( js, css, images, dev )
--- views/templates/barra.php
+++ views/templates/barra.php
@@ -1,5 +1,11 @@
+
+<nav class="barra-mobile">
+    <h1>UpTask</h1>
+    <div class="menu">
+        <img id="mobile-menu" src="build/img/menu.svg">
+    </div>
+</nav>
 <div class="barra">
     <p>Hola: <span><?php echo $_SESSION['nombre'];?></span></p>
     <a href="/logout" class="cerrar-session">Cerrar Sesión</a>
-
 </div>
