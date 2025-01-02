1d2b307997d727aee1c1fbd731fbe534a58a08ae
Bernabep <bernabepando@gmail.com>
2025-01-02 22:16:55 +0100

    pc principal

--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,7 @@
 <?php
 
-$db = mysqli_connect('192.168.0.90', 'remote', 'root', 'uptask_mvc');
+// $db = mysqli_connect('192.168.0.89', 'remote', 'root', 'uptask_mvc');
+$db = mysqli_connect('89.58.31.246', 'appsalon-bpandofdesarrollador', 'T9LCZ)fy98_-zrm29M', 'appsalon_bpandofdesarrollador_uptask_mvc');
 
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
